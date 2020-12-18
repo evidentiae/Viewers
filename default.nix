a64a0234219dcc3d73084f21df41c41e8157ced3
David Waern <david.waern@gmail.com>
2020-12-18 12:29:07 +0100

    default.nix

---
+++ default.nix
@@ -0,0 +1,37 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+with pkgs.lib;
+
+pkgs.stdenv.mkDerivation {
+  name = "oryx-dicom-viewer";
+
+  src = builtins.filterSource (path: type:
+    let p = removePrefix (toString ./. + "/") (toString path); in
+    hasPrefix "platform/viewer/dist"
+  ) ./.;
+  #  let p = removePrefix (toString ./. + "/") (toString path); in
+  #  p == "aliases.config.js" ||
+  #  p == "babel.config.js" ||
+  #  p == "eslintAliasesResolver.js" ||
+  #  p == "jest.config.base.js" ||
+  #  p == "jest.config.js" ||
+  #  p == "postcss.config.js" ||
+  #  p == "lerna.json" ||
+  #  p == "netlify.toml" || #  p == "package.json" ||
+  #  p == "yarn.lock" ||
+  #  p == ".browserslistrc" ||
+  #  p == ".codecov.yml" ||
+  #  p == ".eslintrc.json" ||
+  #  p == ".prettierrc" ||
+    
+  #  hasPrefix ".webpack" p ||
+  #  hasPrefix ".netlify" p ||
+  #  hasPrefix "platform" p ||
+  #  hasPrefix "extensions" p
+  #) ./.;
+
+  installPhase = ''
+    cp -r platform/viewer/dist $out
+  '';
+}
+
