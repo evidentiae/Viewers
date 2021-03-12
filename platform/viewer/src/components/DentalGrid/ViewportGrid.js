abf35553489de3197211a52f14bbccd9d3fe3af3
David Waern <david.waern@gmail.com>
2021-03-12 12:20:53 +0100

    fix

--- platform/viewer/src/components/DentalGrid/ViewportGrid.js
+++ platform/viewer/src/components/DentalGrid/ViewportGrid.js
@@ -33,6 +33,7 @@ const ViewportGrid = function (props) {
   if (activeDisplaySet) {
     var numFrames = activeDisplaySet.numImageFrames;
 
+    layout.viewports = [];
     for (var i=1; i<numFrames; i++) {
       layout.viewports.push({});
     }
