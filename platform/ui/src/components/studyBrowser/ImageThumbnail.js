f8ad29ede4b7d9f39387b0ac2a783371c23e5f27
David Waern <david.waern@gmail.com>
2021-03-29 19:06:52 +0200

    another attempt

--- platform/ui/src/components/studyBrowser/ImageThumbnail.js
+++ platform/ui/src/components/studyBrowser/ImageThumbnail.js
@@ -55,7 +55,7 @@ function ImageThumbnail(props) {
       })
       .catch(error => {
         console.log(error);
-        if (error.isCanceled) return;
+        //if (error.isCanceled) return;
         setLoading(false);
         setError(true);
         throw new Error(error);
