9ce825425d41d59585426e2993520b33d5e1a53a
David Waern <david.waern@gmail.com>
2021-04-07 14:04:51 +0200

    de-comment stackable instances decl

--- platform/core/src/classes/metadata/StudyMetadata.js
+++ platform/core/src/classes/metadata/StudyMetadata.js
@@ -156,7 +156,7 @@ export class StudyMetadata extends Metadata {
     // Split Multi-frame instances and Single-image modalities
     // into their own specific display sets. Place the rest of each
     // series into another display set.
-    //const stackableInstances = [];
+    const stackableInstances = [];
     var i = 0;
     series.forEachInstance(instance => {
       // All imaging modalities must have a valid value for SOPClassUID (x00080016) or Rows (x00280010)
