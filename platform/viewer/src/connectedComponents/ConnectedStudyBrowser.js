58a0952482de700d372c6b5e9ed59b8f2b8f0ca1
David Waern <david.waern@gmail.com>
2021-04-07 15:54:00 +0200

    do not change numImageFrames based on state

--- platform/viewer/src/connectedComponents/ConnectedStudyBrowser.js
+++ platform/viewer/src/connectedComponents/ConnectedStudyBrowser.js
@@ -17,7 +17,6 @@ const mapStateToProps = (state, ownProps) => {
   // progress bar is updated
   const stackLoadingProgressMap = state.loading.progress;
   const studiesWithLoadingData = cloneDeep(ownProps.studies);
-  const numViewports = state.viewports.layout.viewports.length;
 
   studiesWithLoadingData.forEach(study => {
     study.thumbnails.forEach(data => {
@@ -31,7 +30,6 @@ const mapStateToProps = (state, ownProps) => {
       }
 
       data.stackPercentComplete = stackPercentComplete;
-      data.numImageFrames = numViewports;
     });
   });
 
