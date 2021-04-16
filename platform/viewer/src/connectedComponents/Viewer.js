868ab158759237c8f9b9185af8a1da37aa89ca69
David Waern <david.waern@gmail.com>
2021-04-16 09:59:35 +0200

    tweak

--- platform/viewer/src/connectedComponents/Viewer.js
+++ platform/viewer/src/connectedComponents/Viewer.js
@@ -329,18 +329,18 @@ class Viewer extends Component {
                 />
               ) : (
                 <div>
-                  <div className="ToolbarRow">
-                    <ToolbarButton
-                      label="Add study"
-                      icon='maximize'
-                      isActive={true}
-                      onClick={alert("test")}
-                    />
-                  </div>
                   <ConnectedStudyBrowser
                     studies={this.state.thumbnails}
                     studyMetadata={this.props.studies}
                   />
+                  <div className="ToolbarRow">
+                    <ToolbarButton
+                      label="New study"
+                      icon='th'
+                      isActive={true}
+                      onClick={alert("test")}
+                    />
+                  </div>
                 </div>
               )}
             </SidePanel>
