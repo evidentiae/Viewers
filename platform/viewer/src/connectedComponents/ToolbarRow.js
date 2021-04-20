ce9571382e1f0cb77ceb3d356a57aebdee14bf32
David Waern <david.waern@gmail.com>
2021-04-20 16:54:23 +0200

    test number of studies

--- platform/viewer/src/connectedComponents/ToolbarRow.js
+++ platform/viewer/src/connectedComponents/ToolbarRow.js
@@ -201,7 +201,7 @@ class ToolbarRow extends Component {
             label="New study"
             icon='th'
             isActive={true}
-            onClick={() => alert("test")}
+            onClick={() => alert(this.props.studies.length.toString)}
           />
           {buttonComponents}
           <ToolbarButton
