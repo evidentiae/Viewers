ff8b044790eb84b6ea640b412685d14bd1beee93
David Waern <david.waern@gmail.com>
2021-04-21 10:18:41 +0200

    logging

--- platform/viewer/src/connectedComponents/Viewer.js
+++ platform/viewer/src/connectedComponents/Viewer.js
@@ -242,6 +242,9 @@ class Viewer extends Component {
       });
     });
 
+    console.log("render");
+    console.log(this.state.isSelectingLayout);
+
     return (
       <>
         {/* TOOLBAR */}
@@ -284,7 +287,7 @@ class Viewer extends Component {
               this.setState(updatedState);
             }}
             handleMaximize={this.props.onMaximize}
-            handleNewStudy={() => { this.state.isSelectingLayout = true; }}
+            handleNewStudy={() => { alert('foo'); this.state.isSelectingLayout = true; }}
             studies={this.props.studies}
             maximized={this.props.maximized}
           />
