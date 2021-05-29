061b5b6ff1df9f32da587ddcdd8ba1b8834e6273
David Waern <david.waern@gmail.com>
2021-05-29 18:05:49 +0200

    build

--- extensions/cornerstone/src/OHIFCornerstoneViewport.js
+++ extensions/cornerstone/src/OHIFCornerstoneViewport.js
@@ -187,6 +187,7 @@ class OHIFCornerstoneViewport extends Component {
   }
 
   shouldComponentUpdate(nextProps, nextState) {
+    console.log("shouldComponentUpdate OHIFCornerstoneViewport");
     console.log(nextProps);
     console.log(nextState);
     return true;
