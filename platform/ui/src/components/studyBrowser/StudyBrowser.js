1cce140d6c2f4fe8fc9d6f0bafdddcf268f4e0ec
David Waern <david.waern@gmail.com>
2021-06-08 13:43:40 +0200

    fix closure bug

--- platform/ui/src/components/studyBrowser/StudyBrowser.js
+++ platform/ui/src/components/studyBrowser/StudyBrowser.js
@@ -63,7 +63,8 @@ function StudyBrowser(props) {
                     stackPercentComplete={stackPercentComplete}
                     // Events
                     onClick={() => {
-                      setActiveSeriesIndex(seriesIndex);
+                      var index = seriesIndex;
+                      setActiveSeriesIndex(index);
                       onThumbnailClick(displaySetInstanceUID, SeriesInstanceUID);
                     }}
                     onDoubleClick={onThumbnailDoubleClick}
