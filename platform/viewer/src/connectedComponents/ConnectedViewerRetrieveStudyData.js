2b90b56c7657e8c5e6a5ed0951d43c952e0fbabb
David Waern <david.waern@gmail.com>
2021-06-14 15:35:47 +0200

    make doneLoadingStudies more general

--- platform/viewer/src/connectedComponents/ConnectedViewerRetrieveStudyData.js
+++ platform/viewer/src/connectedComponents/ConnectedViewerRetrieveStudyData.js
@@ -26,64 +26,86 @@ const mapDispatchToProps = dispatch => {
     doneLoadingStudies: (studies, activeSeries) => {
       console.log("doneLoadingStudies()");
       console.log(studies);
-      if (!activeSeries && studies.length > 0 && studies[0].displaySets.length > 0) {
-        var displaySets = studies[0].displaySets;
-        var numFrames = 0;
-        for (var i = 0; i < displaySets.length; i++) {
-          if (!displaySets[i].Maximized) numFrames++;
+
+      var studyUID = null;
+      var seriesUID = activeSeries;
+      var displaySets;
+      var found = false;
+      if (seriesUID) {
+        for (var i=0; i<studies.length; i++) {
+          for (var j=0; j<studies[i].series.length; j++) {
+            if (studies[i].series[j].SeriesInstanceUID === seriesUID) {
+              studyUID = studies[i].StudyInstanceUID;
+              displaySets = studies[i].displaySets;
+              found = true;
+              break;
+            }
+          }
+          if (found) break;
         }
+      }
+      if (!seriesUID && studies.length > 0 && studies[0].series.length > 0) {
+        studyUID = studies[0].StudyInstanceUID;
+        seriesUID = studies[0].series[0].SeriesInstanceUID;
+        displaySets = studies[0].displaySets;
+      }
 
-        // XXX: copied from ConnectedStudyBrowser
-        // XXX: clean up, handle all cases, and share code
+      var numFrames = 0;
+      for (var i = 0; i < displaySets.length; i++) {
+        if (!displaySets[i].Maximized) numFrames++;
+      }
 
-        var numRows = 2;
-        var numColumns = 2;
+      // XXX: copied from ConnectedStudyBrowser
+      // XXX: clean up, handle all cases, and share code
 
-        if (numFrames == 1) {
-          numRows = 1;
-          numColumns = 1;
-        } else if (numFrames == 2) {
-          numRows = 1;
-          numColumns = 2;
-        } else if (numFrames == 3) {
-          numRows = 1;
-          numColumns = 3;
-        } else if (numFrames == 4) {
-          numRows = 2;
-          numColumns = 2;
-        } else if (numFrames == 5) {
-          numRows = 2;
-          numColumns = 3;
-        } else if (numFrames == 6) {
-          numRows = 2;
-          numColumns = 3;
-        } else if (numFrames == 7) {
-          numRows = 2;
-          numColumns = 4;
-        } else if (numFrames == 8) {
-          numRows = 2;
-          numColumns = 4;
-        } else if (numFrames == 9) {
-          numRows = 3;
-          numColumns = 3;
-        } else if (numFrames == 10) {
-          numRows = 3;
-          numColumns = 4;
-        }
+      var numRows = 2;
+      var numColumns = 2;
 
-        var viewports = [];
-        for (var i=0; i<numFrames; i++) {
-          viewports.push({plugin: "cornerstone"});
-        }
-        dispatch(setViewportLayoutAndData(
-          {numRows: numRows, numColumns: numColumns, viewports: viewports},
-          displaySets
-        ));
+      if (numFrames == 1) {
+        numRows = 1;
+        numColumns = 1;
+      } else if (numFrames == 2) {
+        numRows = 1;
+        numColumns = 2;
+      } else if (numFrames == 3) {
+        numRows = 1;
+        numColumns = 3;
+      } else if (numFrames == 4) {
+        numRows = 2;
+        numColumns = 2;
+      } else if (numFrames == 5) {
+        numRows = 2;
+        numColumns = 3;
+      } else if (numFrames == 6) {
+        numRows = 2;
+        numColumns = 3;
+      } else if (numFrames == 7) {
+        numRows = 2;
+        numColumns = 4;
+      } else if (numFrames == 8) {
+        numRows = 2;
+        numColumns = 4;
+      } else if (numFrames == 9) {
+        numRows = 3;
+        numColumns = 3;
+      } else if (numFrames == 10) {
+        numRows = 3;
+        numColumns = 4;
+      }
 
-        if (studies[0].series.length > 0) {
-          console.log("dispatching setActiveSeries");
-          dispatch(setActiveSeries(studies[0].series[0].SeriesInstanceUID));
-        }
+      var viewports = [];
+      for (var i=0; i<numFrames; i++) {
+        viewports.push({plugin: "cornerstone"});
+      }
+
+      dispatch(setViewportLayoutAndData(
+        {numRows: numRows, numColumns: numColumns, viewports: viewports},
+        displaySets
+      ));
+
+      if (seriesUID) {
+        console.log("dispatching setActiveSeries");
+        dispatch(setActiveSeries(seriesUID));
       }
     }
   };
