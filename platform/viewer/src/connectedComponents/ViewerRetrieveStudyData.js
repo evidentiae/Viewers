b262eae9a04d35a0e563e5b95f64e32a84707d95
David Waern <david.waern@gmail.com>
2021-06-24 11:30:30 +0200

    skip metadata purge

--- platform/viewer/src/connectedComponents/ViewerRetrieveStudyData.js
+++ platform/viewer/src/connectedComponents/ViewerRetrieveStudyData.js
@@ -423,7 +423,7 @@ function ViewerRetrieveStudyData({
   useEffect(() => {
     if (reloadStudies) {
       console.log("RELOADING STUDIES");
-      studyMetadataManager.purge();
+      //studyMetadataManager.purge();
       purgeCancellablePromises();
       cancelableSeriesPromises = {};
       cancelableStudiesPromises = {};
