e9b1d512a4b5bc02f9ccbbb1ca0a413871e8d235
David Waern <david.waern@gmail.com>
2021-06-24 12:10:24 +0200

    skip loading  remaining series

--- platform/viewer/src/connectedComponents/ViewerRetrieveStudyData.js
+++ platform/viewer/src/connectedComponents/ViewerRetrieveStudyData.js
@@ -291,6 +291,9 @@ function ViewerRetrieveStudyData({
         _updateStudyDisplaySets(study, studyMetadata);
         _updateStudyMetadataManager(study, studyMetadata);
 
+        /*
+        // Not necessary when sync loading
+
         // Attempt to load remaning series if any
         cancelableSeriesPromises[study.StudyInstanceUID] = makeCancelable(
           loadRemainingSeries(studyMetadata)
@@ -303,7 +306,6 @@ function ViewerRetrieveStudyData({
               studyDidLoad(study, studyMetadata, filters);
             }
             // result is null on sync loading
-            //doneLoadingStudies(studies, activeSeries);
           })
           .catch(error => {
             if (error && !error.isCanceled) {
@@ -314,11 +316,13 @@ function ViewerRetrieveStudyData({
           .finally(() => {
             setIsStudyLoaded(true);
           });
+        */
 
         return study;
       });
 
       setStudies(studies);
+      doneLoadingStudies(studies, activeSeries);
     }
   };
 
