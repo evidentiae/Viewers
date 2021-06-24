155d60c16e79b6c45bc0a63bb9afa40ecaa4b57d
David Waern <david.waern@gmail.com>
2021-06-24 16:34:43 +0200

    disable shouldComponentUpdate on ohif cornerstone viewport

--- extensions/cornerstone/src/OHIFCornerstoneViewport.js
+++ extensions/cornerstone/src/OHIFCornerstoneViewport.js
@@ -185,11 +185,13 @@ class OHIFCornerstoneViewport extends Component {
     }
   }
 
+  /*
   shouldComponentUpdate(nextProps, nextState) {
     //return !(this.props.viewportData === nextProps.viewportData);
     //return false;
     return true;
   }
+  */
 
   render() {
     console.log("OHIFCornerstoneViewport render()");
